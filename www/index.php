d6d4916827aa75a6aee03f7813a78e5675489b82
SansHDxXD <89485721+SansHDxXD@users.noreply.github.com>
2022-03-07 15:35:48 -0500

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChatApp</title>
+</head>
+<body>
+
+      <div id="cont">
+          <div id="caj-chat">
+               <div id="chat">
+                   <div id="datoschat">
+                       <span>UsuarioXD</span>
+                       <span>Hola como tas</span>
+                       <span>10:04 am</span>
+                   </div>
+               </div>
+          </div>
+          <form method="POST" action="index.php">
+                 <input type="text" name="nombre" placeholder="pon tu nombre sisiXD">
+                 <textarea name="mensaje" placeholder="ponga su mensajeXD"></textarea>
+                 <input type="submit" name="enviar" value="enviar">
+          </form>
+      </div>
+    
+</body>
+</html>
